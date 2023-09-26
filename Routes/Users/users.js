572b5959085089c8d41100c6b551e37802dce21a
Augah2001 <Augahmups4@gmail.com>
2023-09-27 01:26:12 +0200

    password hash and salting

--- Routes/Users/users.js
+++ Routes/Users/users.js
@@ -3,6 +3,7 @@ const {User} = require('../../models/Users/Users.js');
 const {validateUser} = require('../../models/Users/Users.js')
 const express = require('express');
 const Router = express.Router();
+const bcrypt = require('bcrypt')
 
 
 Router.get('/', async (_,res) => {
@@ -24,7 +25,11 @@ Router.post('/', async (req,res) => {
     if (error) return res.status(400).json({message: error.message})
     let user = await User.findOne({email: req.body.email})
     if (user) return res.status(400).json({message: 'User already exists'})
-    user = new User(req.body)
+    user = new User(_.pick(req.body, ['name', 'username', 'password', 'email']));
+    const salt = await bcrypt.genSalt(10);
+    const hashedpassword = await bcrypt.hash(user.password,salt)
+    user.password = hashedpassword
+
     const result = await user.save()
     res.json(_.pick(result, ['_id', 'name', 'email']))
 
